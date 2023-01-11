4f005ac180572b7113e03fcdb0cd960ffeec77bc
Wendy Wilgus <wilguswendy@gmail.com>
2023-01-11 11:57:25 -0500

    add props and removeCharacter

--- App.js
+++ App.js
@@ -2,13 +2,47 @@ import React, { Component } from "react";
 import Table from './Table';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="container">
-                <Table />
-            </div>
-        )
+    state = {
+        characters: [
+            {
+              name: 'Charlie',
+              job: 'Janitor',
+            },
+            {
+              name: 'Mac',
+              job: 'Bouncer',
+            },
+            {
+              name: 'Dee',
+              job: 'Aspring actress',
+            },
+            {
+              name: 'Dennis',
+              job: 'Bartender',
+            },
+        ]
+    }
+    removeCharacter = (index) => {
+        const { characters } = this.state
+      
+        this.setState({
+          characters: characters.filter((character, i) => {
+            return i !== index
+          }),
+        })
+      }
+    //now pass the data through to the child componenet (Table) with properties.  Now access in Table.js
+    render () {
+        const { characters } = this.state
+
+    
+    return (
+        <div className="container">
+            <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        </div>
+    )
     }
 }
+
 
 export default App;
--- Table.js
+++ Table.js
@@ -12,39 +12,32 @@ const TableHeader = () => {
         )
     }
 
-const TableBody = () => {
-    return (
-        <tbody>
-            <tr>
-                <td>Charlie</td>
-                <td>Janitor</td>
+    //simple Component.  data stored in App.js.  Pass the props through as a parameter, and map through the array to return a table row for each object in the array. 
+const TableBody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            //always use keys when making lists in React to help identify each list item
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
             </tr>
-            <tr>
-                <td>Mac</td>
-                <td>Bouncer</td>
-            </tr>
-            <tr>
-                <td>Dee</td>
-                <td>Aspiring Actress</td>
-            </tr>
-            <tr>
-                <td>Dennis</td>
-                <td>Bartender</td>
-            </tr>
-        </tbody>
-    )
+        )
+    })
+    return <tbody>{rows}</tbody> 
 }
 
-class Table extends Component {
-    render() {
-        return (
-            <table>
-                <TableHeader />
-                <TableBody  />
-            </table>
-        )
+//class componentne must contain render
+const Table = (props) => {
+      const { characterData, removeCharacter } = props
+  
+      return (
+        <table>
+          <TableHeader />
+          <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+        </table>
+      )
     }
-}
+  
                 
 
 export default Table;
